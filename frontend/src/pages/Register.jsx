b4dae4c1355a6a68d5fc9c6cba97d0ce16afb602
Nitish mahajan <nitishmahajanlegend2@gmail.com>
2026-03-16 04:04:26 -0700

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -9,7 +9,7 @@ const Register=()=>{
     const submitHandler=async(values)=>{
        try{
         setLoading(true)
-        await axios.post('http://localhost:8080/api/v1/users/register',values)
+        await axios.post('https://pythoncodeeditor-48vl.onrender.com/api/v1/users/register',values)
         message.success('Registration successfull')
         setLoading(false)
         navigate('/login')
