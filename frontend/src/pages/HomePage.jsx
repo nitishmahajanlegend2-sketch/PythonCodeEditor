b701d83f939e377d72cfd50c6da065f91095b901
Nitish mahajan <nitishmahajanlegend2@gmail.com>
2026-03-16 04:06:42 -0700

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -19,7 +19,7 @@ const HomePage = () => {
   const getcode=async(file)=>{
         const username=localStorage.getItem('user');
         const jsonusername=JSON.parse(username)
-        const response= await fetch('http://localhost:8080/api/handlecode/getfilecode/', {
+        const response= await fetch('https://pythoncodeeditor-48vl.onrender.com/api/handlecode/getfilecode/', {
           method:'POST',
         // Use uppercase 'POST' (convention)
         headers: {
